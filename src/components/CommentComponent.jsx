a3a82a0f2d0c3bfbbca2d7cb91aa368139117457
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-09-09 11:19:32 +0200

    Working on showing comments correctly

--- src/components/CommentComponent.jsx
+++ src/components/CommentComponent.jsx
@@ -1,17 +1,26 @@
 import PostUserProfile from "./PostUserProfile";
 import dateConversion from "../utils/dateConversion";
+import "../styles/commentcomponent.css";
+import like from "../assets/like.svg";
 
 function CommentComponent(props) {
   let tempDate = dateConversion(props.comment.commentDate);
 
   return (
     <div className="commentComponent">
-      <PostUserProfile
-        profilePic={props.comment.user.profilePic}
-        userName={props.comment.user.username}
-      />
-      <div className="postedCommentOnDate">{tempDate}</div>
+      <div className="commentTop">
+        <PostUserProfile
+          profilePic={props.comment.user.profilePic}
+          userName={props.comment.user.username}
+        />
+        <div className="postedCommentOnDate">{tempDate}</div>
+      </div>
       <div className="commentText">{props.comment.text}</div>
+      <button className="likeCommentButton">
+        <img className="likeBtnImg" src={like}></img>
+        <div className="btnText">Like</div>
+      </button>
+      <hr />
     </div>
   );
 }

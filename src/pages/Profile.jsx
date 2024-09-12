eb0d042a6d5a3f861c9813940d15b4a0ef17ea8d
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-09-12 17:20:28 +0200

    Posts by user displaying nicely

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,12 +3,27 @@ import useRedirectValidFail from "../hooks/useRedirectValidFail";
 import { AuthContext } from "../App";
 import "../styles/profile.css";
 import person from "../assets/person.svg";
+import Post from "../components/Post";
+import getPostsByUser from "../services/getPostsByUser";
 
 function Profile() {
   const [showPosts, setShowPosts] = useState(true);
   const [showComments, setShowComments] = useState(false);
+  const [userPosts, setUserPosts] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [loading, setLoading] = useState(true);
   const authContext = useContext(AuthContext);
   let img;
+
+  // load posts on component load
+  useEffect(() => {
+    const loadPosts = async () => {
+      const posts = await getPostsByUser(limit);
+      setUserPosts(posts);
+      setLoading(false);
+    };
+    loadPosts();
+  }, []);
 
   useRedirectValidFail(authContext.err);
   console.log(authContext.user);
@@ -33,7 +48,24 @@ function Profile() {
             <button className="postsBtnProf">Posts</button>
             <button className="commentsBtnProf">Comments</button>
           </div>
-          <div className={"profilePostsContainer " + showPosts}></div>
+          <div className={"profilePostsContainer " + showPosts}>
+            {loading ? <div>LOADING ...</div> : null}
+            {userPosts.map((item) => (
+              <Post
+                text={item.text}
+                imageUrl={item.imageUrl}
+                key={item.id}
+                userName={item.user.username}
+                userid={item.user.id}
+                profilePic={item.user.profilePic}
+                postDate={item.postDate}
+                postid={item.id}
+                likeCount={item.likes.length}
+                likeUsers={item.likes}
+                commentCount={item._count.comments}
+              ></Post>
+            ))}
+          </div>
         </div>
       </div>
     </>

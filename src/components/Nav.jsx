4e35b292d89dd64d64ee11891c0b8943b6c40fba
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-09-15 14:12:25 +0200

    Working on styling of the page somewhat more

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,46 @@
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import home from "../assets/home.svg";
 import friends from "../assets/friends.svg";
 import logout from "../assets/logoutt.svg";
 import "../styles/nav.css";
 import { AuthContext } from "../App";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 function Nav() {
   const authContext = useContext(AuthContext);
   const navigate = useNavigate();
+  const [activeFriends, setActiveFriends] = useState(false);
+  const [activeHome, setActiveHome] = useState(false);
+  const [activeProfile, setActiveProfile] = useState(false);
+
+  if (window.location.pathanme === "/friends") {
+    console.log("xd");
+  } else if (window.location.pathanme === "") {
+    console.log("dd");
+  }
+
+  useEffect(() => {
+    console.log("///////////////////////////////");
+    console.log("///////////////////////////////");
+    console.log("///////////////////////////////");
+    console.log("///////////////////////////////");
+    console.log("///////////////////////////////");
+    console.log("changed pathname");
+
+    if (window.location.pathname === "/friends") {
+      setActiveFriends(true);
+      setActiveHome(false);
+      setActiveProfile(false);
+    } else if (window.location.pathname === "/") {
+      setActiveFriends(false);
+      setActiveHome(true);
+      setActiveProfile(false);
+    } else if (window.location.pathname === "/profile") {
+      setActiveFriends(false);
+      setActiveHome(false);
+      setActiveProfile(true);
+    }
+  }, [window.location.pathname]);
 
   const handleHomeClick = () => {
     return navigate("/");
@@ -29,13 +61,13 @@ function Nav() {
 
   return (
     <div className="navBar">
-      <button className="btn1" onClick={handleHomeClick}>
+      <button className={"btn1 " + activeHome} onClick={handleHomeClick}>
         <img src={home} className="navIcon"></img>
       </button>
-      <button className="btn2" onClick={handleFriendsClick}>
+      <button className={"btn2 " + activeFriends} onClick={handleFriendsClick}>
         <img src={friends} className="navIcon"></img>
       </button>
-      <button className="btn3" onClick={handleProfClick}>
+      <button className={"btn3 " + activeProfile} onClick={handleProfClick}>
         <img src={authContext.user.profilePic} className="profileNav"></img>
         Profile
       </button>
